b4781cd699dd0a7a2b4444575d3cb43db2852252
Reda Mansy <mansyr@cardiff.ac.uk>
2023-06-12 18:17:37 +0100

    Added post creation code.

--- PortfolioApp/routes.py
+++ PortfolioApp/routes.py
@@ -1,34 +1,29 @@
 from PortfolioApp import app
-from flask import render_template
-from flask_mail import Mail, Message
+from flask import render_template, Blueprint, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email
-import email_validator
 
-app.config.update(dict(
-    MAIL_SERVER = 'smtp.googlemail.com',
-    MAIL_PORT = 465,
-    MAIL_USE_TLS = False,
-    MAIL_USE_SSL = True,
-    MAIL_USERNAME = 'ridamansy',
-    MAIL_PASSWORD = '[password]'
-))
 
-mail = Mail(app)
+
+# app = Blueprint("posts", __name__)
 
 
 
 @app.route('/')
 @app.route('/home', methods=["GET", "POST"])
 def home():
-    cform = contactForm()
-    return render_template('index.html', form=cform)
+    return render_template('index.html')
 
+@app.get("/post/<string:title>")
+def display_post(title: str):
+    return "Display post page"
 
-class contactForm(FlaskForm):
-    name = StringField(label='Name', validators=[DataRequired()])
-    email = StringField(label='Email', validators=[
-    DataRequired(), Email(granular_message=True)])
-    message= StringField(label='Message')
-    submit = SubmitField(label="Submit")
+@app.route('/create_post', methods=["GET", "POST"])
+def create_post():
+    if request.method == "POST":
+        title = request.form.get("title")
+        content = request.form.get("content")
+
+        return redirect(url_for(".display_post", title=title))
+    return render_template("new_post.html")
