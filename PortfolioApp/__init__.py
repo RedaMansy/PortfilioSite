950e04ec2606f1bba0af8d4cf1112852b9f6ed5a
Reda Mansy <mansyr@cardiff.ac.uk>
2023-06-12 20:27:01 +0100

    Fixed routing to and from blog

--- PortfolioApp/__init__.py
+++ PortfolioApp/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 from .routes.index import index_blueprint
 from .routes.create_post import post_pages
+from .routes.blog import blog_blueprint
 
 
 app = Flask(__name__)
 
 app.register_blueprint(post_pages)
 app.register_blueprint(index_blueprint)
+app.register_blueprint(blog_blueprint)
 
---
+++ PortfolioApp/routes/blog.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, redirect, url_for, render_template, request
+
+blog_blueprint = Blueprint("blog", __name__)
+
+@blog_blueprint.route("/blog", methods=["GET", "POST"])
+def blog():
+    return render_template("blog.html")
