8a79c6642d9f15195564ffac2d765d0c7f025a46
Reda Mansy <mansyr@cardiff.ac.uk>
2023-06-17 23:47:20 +0100

    color changing navbar to new style.

---
+++ PortfolioApp/static/js/script.js
@@ -0,0 +1,59 @@
+// const header = document.getElementsByTagName(nav);
+
+// window.onscroll = function() {
+//     var top = window.scrollY;
+//     if(top >=100) {
+//         header.classList.add('navbar-light');
+//         header.classList.remove('navbar-dark');
+
+//     }
+//     else {
+//         header.classList.remove('navbar-light');
+//         header.classList.add('navbar-dark');
+//     }
+// }
+
+
+// document.onscroll = function() {
+//     const nav = document.querySelector("nav");
+//     const home = document.getElementById("home");
+    
+//     if(home.getBoundingClientRect().top <= 0) { // if the distance of the 'specs' section to the browser top is smaller than 0
+//       nav.classList.add('navbar-light'); // add dark font color
+//       nav.classList.remove('navbar-dark')
+//       console.log(nav.classList)
+//     } else {
+//     console.log(nav.classList)
+//       nav.classList.remove('navbar-light'); // remove dark  font color
+//       nav.classList.add('navbar-dark')
+//     }
+//   }
+
+// const nav = document.getElementById("mynav");
+
+// window.onscroll = function() {
+//     console.log(nav.classList)
+//     // var top = window.scrollY;
+//     // if(top >=100) {
+//     //     nav.classList.add('navbarDark');
+//     // }
+//     // else {
+//     //     nav.classList.remove('navbarDark');
+//     // }
+// }
+
+document.onscroll = function() {
+    const nav = document.querySelector("nav");
+    const home = document.getElementById("home");
+    var top = window.scrollY;
+    
+    if(top >= 100) { // if the distance of the 'specs' section to the browser top is smaller than 0
+      nav.classList.add('navbar-light'); // add dark font color
+      nav.classList.remove('navbar-dark')
+      console.log(nav.classList)
+    } else {
+    console.log(nav.classList)
+      nav.classList.remove('navbar-light'); // remove dark  font color
+      nav.classList.add('navbar-dark')
+    }
+  }
